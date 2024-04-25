a384153ad5a73b1c6531d8c4ae3c79583585c349
Marble <scnideffer@gmail.com>
2024-04-24 17:22:54 -0700

    created stub functions, source/header files

---
+++ memoryHelper.h
@@ -0,0 +1,12 @@
+//
+// Created by Scooter on 4/24/2024.
+//
+
+#ifndef MEMORYGAME_MEMORYHELPER_H
+#define MEMORYGAME_MEMORYHELPER_H
+
+void shuffle();
+int getInteger();
+bool yesOrNo();
+
+#endif //MEMORYGAME_MEMORYHELPER_H
---
+++ memorySource.h
@@ -0,0 +1,26 @@
+//
+// Created by Scooter on 4/24/2024.
+//
+
+#ifndef MEMORYGAME_MEMORYSOURCE_H
+#define MEMORYGAME_MEMORYSOURCE_H
+
+//Recommended constants
+const int HARD_ROW = 5;
+const int HARD_COL = 6;
+const int EASY_ROW = 4;
+const int EASY_COL = 4;
+const char SPACE = ' ';
+const char UNKNOWN = 'X';
+
+void displayInstructions();
+char createBoard();
+void getMove();
+// Showboard should be Overloaded
+void showBoard();
+bool checkMatch();
+void updateBoard();
+
+
+
+#endif //MEMORYGAME_MEMORYSOURCE_H
